51e94f9bf3086fdedec8c5d468d5b1f509b57458
infam0us94 <ivanpopoff94@mail.ru>
2020-10-01 13:58:52 +0300

    save message in main activity

--- app/src/main/java/com/example/messenger/ui/screens/single_chat/SingleChatFragment.kt
+++ app/src/main/java/com/example/messenger/ui/screens/single_chat/SingleChatFragment.kt
@@ -200,6 +200,7 @@ class SingleChatFragment(private val contact: CommonModel) :
             if (message.isEmpty()) {
                 showToast("Введите сообщение")
             } else sendMessage(message, contact.id, TYPE_TEXT) {
+                saveToMainList(contact.id, TYPE_CHAT)
                 chat_input_message.setText("")
             }
         }
